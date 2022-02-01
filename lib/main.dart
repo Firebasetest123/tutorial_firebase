8f90353b844a5bf3bdc5a9b42ce9604920fac9e3
Firebasetest123 <97272091+Firebasetest123@users.noreply.github.com>
2022-02-01 06:33:13 +0530

    My First App
    This is My First App

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Jagan Todo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Jagan Todo App Home Page'),
     );
   }
 }
